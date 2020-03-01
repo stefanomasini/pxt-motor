26f92a725cbf66d567492eb44797031431a710f5
Stefano Masini <stefano@stefanomasini.com>
2020-03-01 13:48:42 +0100

    bugfix

--- main.ts
+++ main.ts
@@ -257,7 +257,7 @@ namespace motor {
             initPCA9685()
         }
         setStepper_28(Steppers.M1_M2, (direction == TurnDirection.Right) ? Dir.CCW : Dir.CW);
-        setStepper_28(Steppers.M3_M4, (direction == TurnDirection.Right) ? Dir.CW : Dir.CCW);
+        setStepper_28(Steppers.M3_M4, (direction == TurnDirection.Right) ? Dir.CCW : Dir.CW);
         basic.pause(ms);
         motorStopAll();
     }
